dc2fd4487d01212104139c88eb91fd13ffe05790
tu15007959375 <1804694379@qq.com>
2023-03-09 14:52:10 +0800

    leetcode03

---
+++ LeetCode/offer03.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int findRepeatNumber(vector<int>& nums) {
+	unordered_map<int, bool> map;
+    for(int num : nums) 
+	{
+        if(map[num]) return num;
+            map[num] = true;
+    }
+    return -1;
+
+}
+int main()
+{	
+	vector<int> nums={1,4,6,7,2,4};
+	cout<<findRepeatNumber(nums)<<endl;
+	return 0; 
+}
+
