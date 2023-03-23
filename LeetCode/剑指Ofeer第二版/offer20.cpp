bee73af19f1bc95ebed7730c9c7b235e95cc3bcb
tu15007959375 <1804694379@qq.com>
2023-03-23 21:53:49 +0800

    offer20 need twice

--- LeetCode/剑指Ofeer第二版/offer20.cpp
+++ LeetCode/剑指Ofeer第二版/offer20.cpp
@@ -1,13 +1,43 @@
 #include"util.h"
+typedef pair<char,int> charint;
+typedef unordered_map<char,int> unmap;
 
 bool isNumber(string s) {
-	return false;
+	vector<unmap> states = {
+		unmap{charint(' ',0),charint('s',1),charint('d',2),charint('.',4)},
+		unmap{charint('d',2),charint('.',4)},
+		unmap{charint('d',2),charint('.',3),charint('e',5),charint(' ',8)},
+		unmap{charint('d',3),charint('e',5),charint(' ',8)},
+		unmap{charint('d',3)},
+		unmap{charint('s',6),charint('d',7)},
+		unmap{charint('d',7)},
+		unmap{charint('d',7),charint(' ',8)},
+		unmap{charint(' ',8)}
+	};
+	int p = 0;
+	char t;
+	for(char c:s){
+		if(c >= '0' && c <= '9')
+			t = 'd';
+		else if(c == '+' || c == '-')
+			t = 's';
+		else if(c == 'e' || c == 'E')
+			t = 'e';
+		else if(c == '.' || c == ' ')
+			t = c;
+		else
+			t = '?';
+		if(!states[p].count(t))
+			return false;
+		p = (int) states[p][t];
+	}
+	return p == 2 || p == 3 || p == 7 || p == 8;
 }
 
 int main()
 {
 	
-	isNumber("  12e33  ");	
+	cout<<isNumber("  12e33  ");	
 	return 0;
 }
 
