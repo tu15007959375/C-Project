c49f343fe64aa8f3660ec404d408d369bf10ad06
tu15007959375 <1804694379@qq.com>
2023-03-27 19:30:01 +0800

    offer27 other method

--- LeetCode/剑指Ofeer第二版/offer27.cpp
+++ LeetCode/剑指Ofeer第二版/offer27.cpp
@@ -20,12 +20,24 @@ TreeNode* mirrorTree(TreeNode* root) {
 		swapTree(root);
 	return root;
 }
-
+//更加好的递归解法
+TreeNode* mirrorTree2(TreeNode* root) {
+	if (root == NULL)
+		return NULL;
+	
+	TreeNode* left = mirrorTree2(root->left);
+	TreeNode* right = mirrorTree2(root->right);
+	
+	root->left = right;
+	root->right = left;
+	
+	return root;
+}
 int main()
 {
 	TreeNode *A = new TreeNode;
 	createTree(A,{0,3,4,5,1,2},1);//层次遍历建立二叉树
-	A = mirrorTree(A);
+	A = mirrorTree2(A);
 	printTree(A);
 	return 0;
 }
