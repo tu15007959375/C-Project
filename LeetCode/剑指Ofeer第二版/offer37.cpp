40f0880183655924d46d885266011c0fc3723ad2
tu15007959375 <1804694379@qq.com>
2023-04-10 10:56:58 +0800

    offer37 finish

--- LeetCode/剑指Ofeer第二版/offer37.cpp
+++ LeetCode/剑指Ofeer第二版/offer37.cpp
@@ -7,6 +7,7 @@ public:
 		if(!root)
 			return "";
 		queue<TreeNode *> q;
+		vector<int> valvec;
 		string serializestr="";
 		q.push(root);
 		while(!q.empty()){
@@ -14,31 +15,28 @@ public:
 			q.pop();
 			if(tmp)
 			{
-				serializestr +=to_string(tmp->val);
+				valvec.push_back(tmp->val);
 				q.push(tmp->left);
 				q.push(tmp->right);
 			}
 			else
 			{
-				serializestr +=" ";
+				valvec.push_back(1001);
 			}
 		}
+		int i=valvec.size()-1;
+		while(valvec[i]==1001)
+		{
+			valvec.pop_back();
+			i--;
+		}
+		for(int i=0;i<valvec.size();i++)
+			serializestr += to_string(valvec[i])+",";
 		return serializestr;
-	}
-	TreeNode *createTree(string data,int i)
-	{
-		if(i>data.size()-1||data[i]== ' ')
-			return NULL;
-		TreeNode *root=new TreeNode(data[i]-'0');
-		root->left=createTree(data,2*i);
-		root->right=createTree(data,2*i+1);
-		return root;
-		
 	}
 	// Encodes a tree to a single string.
 	string serialize(TreeNode* root) {
 		string serializestr=levelorder(root);
-		serializestr.erase(serializestr.find_last_not_of(" ")+1);
 		return serializestr;
 	}
 	
@@ -46,17 +44,46 @@ public:
 	TreeNode* deserialize(string data) {
 		if(data=="")
 			return NULL;
-		data = " "+data;
-		TreeNode * root = createTree(data,1);
+		vector<int> valvec;
+		queue<TreeNode *> q;
+	
+		int pos,i=1;
+		while(data.size()!=0||data.find(",")!=-1){
+			pos = data.find(",");
+			string tmp = data.substr(0,pos);
+			valvec.push_back(stoi(tmp));
+			data.erase(0,pos+1);
+		}
+		TreeNode * root=new TreeNode(valvec[0]);
+		q.push(root);
+		while(!q.empty()){
+			if(i>valvec.size()-1)
+				break;
+			TreeNode *tmpnode = q.front();
+			q.pop();
+			if(valvec[i]!=1001)
+			{
+				tmpnode->left=new TreeNode(valvec[i]);
+				q.push(tmpnode->left);
+			}
+			i++;
+			if(i<valvec.size()&&valvec[i]!=1001)
+			{
+				tmpnode->right=new TreeNode(valvec[i]);
+				q.push(tmpnode->right);
+			}
+			i++;
+		}
+		
 		return root;
 	}
 };
 
 int main() {
 	TreeNode *root;
-//	createTree(root,{0,1,2,3,-1,-1,4,5},1);
-	createTree(root,{0,1},1);
+	createTree(root,{0,1,2},1);
 	Codec ser, deser;
+	cout<<endl;
 	TreeNode* ans = deser.deserialize(ser.serialize(root));
 	printTree(ans);
 	return 0;
