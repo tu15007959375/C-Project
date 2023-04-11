a20b2f0bba3cbf6f8816bc389377b5d808d10bc2
tu15007959375 <1804694379@qq.com>
2023-04-11 09:44:18 +0800

    offer40 finish

--- LeetCode/剑指Ofeer第二版/offer40.cpp
+++ LeetCode/剑指Ofeer第二版/offer40.cpp
@@ -19,17 +19,35 @@
 //		res.push_back(arr[size--]);
 //	return res;
 //}
-void quicksort(vector<int>& arr,int l,int r)
+vector<int> quicksort(vector<int>& arr,int k,int l,int r)
 {
-	
+	int i=l;
+	int j=r;
+	while(i<j){
+		while(i<j&&arr[j]>=arr[l])
+			j--;
+		while(i<j&&arr[i]<=arr[l])
+			i++;
+		swap(arr[i],arr[j]);
+	}
+	swap(arr[i],arr[l]);
+	if(i>k)
+		return quicksort(arr,k,l,i-1);
+	if(i<k)
+		return quicksort(arr,k,i+1,r);
+	vector<int> res;
+	res.assign(arr.begin(),arr.begin()+k);
+	return res;
 }
 vector<int> getLeastNumbers(vector<int>& arr, int k) {
-	
-	
+	if(k>=arr.size())
+		return arr;
+	vector<int> res = quicksort(arr,k,0,arr.size()-1);
+	return res;
 }
 int main() {
-	vector<int> arr={4,5,1,6,2,7,3,8};
-	vector<int> res = getLeastNumbers(arr,4);
+	vector<int> arr={0,1,2,1};
+	vector<int> res = getLeastNumbers(arr,1);
 	for(int x:res)
 		cout<<x<<"\t";
 	return 0;
