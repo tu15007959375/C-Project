037a22b855a95e06f0272c8c876f6469420f3602
tu15007959375 <1804694379@qq.com>
2023-04-12 09:53:53 +0800

    offer42 finish

--- LeetCode/剑指Ofeer第二版/offer42.cpp
+++ LeetCode/剑指Ofeer第二版/offer42.cpp
@@ -1,7 +1,18 @@
 #include"util.h"
 
 int maxSubArray(vector<int>& nums) {
+	if(nums.size()==0)
+		return 0;
+	if(nums.size()==1)
+		return nums[0];
+	vector<int> dp(nums.size(),0);
+	dp[0]=nums[0];
+	for(int i=1;i<nums.size();i++)
+	{
+		dp[i] = dp[i-1]>0?dp[i-1]+nums[i]:nums[i];
+	}
 	
+	return *max_element(dp.begin(),dp.end());
 }
 int main() {
 	vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
