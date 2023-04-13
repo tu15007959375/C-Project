dc0544e468a86cf2e0292cdc4835ccae968fc2f9
tu15007959375 <1804694379@qq.com>
2023-04-13 12:13:58 +0800

    offer44 need twice

--- LeetCode/剑指Ofeer第二版/offer44.cpp
+++ LeetCode/剑指Ofeer第二版/offer44.cpp
@@ -1,23 +1,25 @@
 #include"util.h"
 
 int findNthDigit(int n) {
-	map<int,int> countmap;
-	map<int,int>::iterator iter;
-	int sum = 0;
-	int i=0;
-	while(sum<n){
-		int tmp = 9*pow(10,i);
-		countmap[i]=tmp;
-		sum += tmp;
+	int i=1;//分段 第1段开始 
+	
+	while(n > 9*pow(10,i-1) *i)//找到n在第几段 ，如第二段有90*2 个字符
+	{
+		n = n - 9*pow(10,i-1) *i;
 		i++;
 	}
-	iter = --countmap.end();
-	cout<<iter->first<<"\t"<<iter->second<<"\t";
-	return 0;
-}
+	//到这里已经知道是第几段i。 且是这一段的第几个字符
+	
+	int min_num = pow(10,i-1);//这一段的最小值。比如第二段，最小为10
+	int cur_num = min_num + (n-1)/i;//知道是属于哪个数字 每个数字长度为i  第0数字是它本身
+	int cur_bit = (n-1)%i;//看是这个数字的第几位，第0位为最高位，第1位为次高位。
+	
+	string s = to_string(cur_num);
+	return   s[cur_bit]-'0';//返回这一位的数字
+}		
 
 int main() {
-	cout<<findNthDigit(200);
+	cout<<findNthDigit(2147483647);
 	return 0;
 }
 
