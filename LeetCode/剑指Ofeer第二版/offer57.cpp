6eba3be6b2793f114f6f876ce817bc22b3f64f03
tu15007959375 <1804694379@qq.com>
2023-04-24 10:36:04 +0800

    offer57 finish

--- LeetCode/剑指Ofeer第二版/offer57.cpp
+++ LeetCode/剑指Ofeer第二版/offer57.cpp
@@ -1,22 +1,77 @@
 #include"util.h"
 
-vector<int> twoSum(vector<int>& nums, int target) {
-	int left = 0, right = nums.size() - 1;
-	while (left < right) {
-		if (nums[left] + nums[right] < target)  // 和太小了，nums[left] 可以再大点
-			left++;
-		else if (nums[left] + nums[right] > target) // 和太大了，nums[right] 可以再小一点
-			right--;
-		else 
-			return vector<int> {nums[left], nums[right]};
+vector<int> findres(int begin,int end,int target)
+{
+	vector<int> re;
+	int sum=0;
+	for(int i=begin;i<=end;i++)
+	{
+		re.push_back(i);
+		sum += i;
+		if(sum == target)
+			return re;
+		else if(sum > target)
+			return {};
 	}
-	return vector<int> {-1, -1};
+	return {};
+}
+//暴力法
+vector<vector<int>> findContinuousSequence1(int target) {
+	if(target==0||target==1||target==2)
+		return {{}};
+	vector<vector<int>> res;
+	int end = (target-1)/2+1;
+	if(target%2==0)
+		end--;
+	int begin=1;
+	while(begin!=end){
+		vector<int> re = findres(begin,end,target);
+		if(re.size())
+			res.push_back(re);
+		begin++;
+	}
+	return res;
+}
+int getsum(int begin,int end)
+{
+	return (begin+end)*(end-begin+1)/2;
+}
+//双指针法
+vector<vector<int>> findContinuousSequence(int target) {
+	if(target==0||target==1||target==2)
+		return {{}};
+	vector<vector<int>> res;
+	int end = (target-1)/2+1;
+	if(target%2==0)
+		end--;
+	int pos1 = 1;
+	int pos2 = 2;
+	while(pos2<=end){
+		if(target>getsum(pos1,pos2))
+			pos2++;
+		else if(target<getsum(pos1,pos2))
+			pos1++;
+		else
+		{
+			vector<int> re;
+			for(int i=pos1;i<=pos2;i++)
+				re.push_back(i);
+			res.push_back(re);
+			pos1++;
+			pos2++;
+		}
+	}
+	return res;
 }
 int main() {
-	vector<int> nums = {2,7,11,15};
-	vector<int> res = twoSum(nums,9);
-	for(int x:res)
-		cout<<x<<"\t";
+
+	vector<vector<int>> res = findContinuousSequence(15);
+	for(vector x:res)
+	{
+		for(int tmp:x)
+			cout<<tmp<<"\t";
+		cout<<endl;
+	}
 	return 0;
 }
 
