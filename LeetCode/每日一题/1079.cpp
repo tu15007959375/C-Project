24bf8ccf8b402c2d6c70078af4987a978017ec54
tu15007959375 <1804694379@qq.com>
2023-05-19 10:47:37 +0800

    1079 带重复的全排列

---
+++ LeetCode/每日一题/1079.cpp
@@ -0,0 +1,34 @@
+#include "util.h"
+
+int dfs(unordered_map<char, int>& count, set<char>& tile, int i) {
+	if (i == 0) {
+		return 1;
+	}
+	int res = 1;
+	for (char t : tile) {
+		if (count[t] > 0) {
+			count[t]--;
+			res += dfs(count, tile, i - 1);
+			count[t]++;
+		}
+	}
+	return res;
+}
+int numTilePossibilities(string tiles) {
+	unordered_map<char, int> count;
+	set<char> tile;
+	int n = tiles.length();
+	for (char c : tiles) {
+		count[c]++;
+		tile.insert(c);
+	}
+	return dfs(count, tile, n) - 1;
+}
+
+
+
+int main()
+{
+	cout<<numTilePossibilities("AAB");
+	return 0;
+}
